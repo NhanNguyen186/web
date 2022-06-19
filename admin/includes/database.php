faa8cd00dba378b376229525db8636c05aa41bc2
nhan <anhdaden186@gmail.com>
2022-06-19 22:06:36 +0700

    Update database.php

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,9 +2,9 @@
 class database{
 
 	//DB Params
-	private $dns = "mysql:host=localhost;dbname=blog2";
-	private $username = "root";
-	private $password = "";
+	private $dns = "mysql:host=bsrmfvqehs1ovj9l7fne-mysql.services.clever-cloud.com;dbname=bsrmfvqehs1ovj9l7fne";
+	private $username = "uzea0cfs7lmx2gsv";
+	private $password = "sqoB7Do0G4qUrDcdoe2R";
 	private $conn;
 
 	//DB Connect
